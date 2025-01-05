a592c3af18e61decfed75febedf2dae6267e675e
Zenith <137868618+ZenithSUS@users.noreply.github.com>
2025-01-05 21:46:31 +0800

    Update greekmythApiAuth.js

--- api/auth/greekmythApiAuth.js
+++ api/auth/greekmythApiAuth.js
@@ -1,5 +1,5 @@
 // URL for the API endpoint
-const url = "http://localhost/GreekMythApi/api/auth.php";
+const url = "https://zenithsus.infinityfreeapp.com/GreekMythApi/api/auth.php";
 
 // Elements of every authorization pages
 const loginForm = document.querySelector('#loginForm');
