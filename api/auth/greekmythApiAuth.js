43c33a9d5d26baae822c001e28536decb4e9927d
Zenith <137868618+ZenithSUS@users.noreply.github.com>
2025-01-17 19:16:47 +0800

    Update greekmythApiAuth.js

--- api/auth/greekmythApiAuth.js
+++ api/auth/greekmythApiAuth.js
@@ -187,6 +187,9 @@ const startTimer = (button, duration) => {
 const submitData = async (formData, button) => {
     const response = await fetch(url, {
         method: "POST",
+        headers: {
+            "Access-Control-Allow-Origin": "*"
+        },
         body: formData,
     });
 
